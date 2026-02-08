smx-smx/yafex

commit 5ce3eb3957e99ee1e66ca1c2c9cb61fdd559e391

Refactor LzhsChunk and simplify code

Yafex/FileFormats/LzhsFs/LzhsFsExtractor.cs
@@ -59,7 +59,7 @@ namespace Yafex.FileFormats.LzhsFs
             var size = UNCOMPRESSED_HEADING_SIZE;
             foreach (var chunk in GetChunks())
             {
-                size += chunk.outerHeader.uncompressedSize;
+                size += chunk.SizeUncompressed;
             }
             return size;
         }
@@ -75,23 +75,14 @@ namespace Yafex.FileFormats.LzhsFs
 
             while (inOffset < data.Length)
             {
-                var outer = new LzhsHeader(span.Slice(inOffset, lzhsHeaderSize));
-                // outer header contains segment number instead of checksum
-                var chunkNo = outer.checksum;
-
-                // compressed size excludes header size
-                var chunkTotalSize = outer.compressedSize + lzhsHeaderSize;
-
-                var lzhsStart = inOffset + lzhsHeaderSize;
-                var lzhsSize = (int)Pad((uint)(chunkTotalSize - lzhsHeaderSize));
-
-                var chunkBuf = data.Slice(lzhsStart, lzhsSize);
-
-                var chunk = new LzhsChunk(chunkNo, lzhsSize, outOffset, outer, chunkBuf);
+                var chunk = new LzhsChunk(data, inOffset, outOffset);
+                if(chunk.SizeCompressed == 0)
+                {
+                    yield break;
+                }
+                inOffset += chunk.SegmentSize;
+                outOffset += chunk.SizeUncompressed;
                 yield return chunk;
-
-                inOffset = lzhsStart + lzhsSize;
-                outOffset += chunk.outerHeader.uncompressedSize;
             }
         }
     }
@@ -109,19 +100,18 @@ namespace Yafex.FileFormats.LzhsFs
 
         private void DumpFailedChunk(LzhsChunk chunk)
         {
-            string dumpPath = Path.Combine(Path.GetTempPath(), $"lzhs_{chunk.index}.bin");
+            string dumpPath = Path.Combine(Path.GetTempPath(), $"lzhs_{chunk.SegmentIndex}.bin");
             log.Debug($"Failed chunk dumped: {dumpPath}");
-            File.WriteAllBytes(dumpPath, chunk.buf.ToArray());
+            File.WriteAllBytes(dumpPath, chunk.SegmentData.ToArray());
         }
 
         public void WriteChunk(LzhsChunk chunk)
         {
-            Trace.WriteLine($"{chunk.index}: {chunk.size}");
-            var ptrOut = mfOut.Data.Span.Slice(chunk.outputOffset);
+            var ptrOut = mfOut.Data.Span.Slice(chunk.OutputOffset);
 
-            if (chunk.isUncompressed)
+            if (chunk.IsUncompressed)
             {
-                var raw = chunk.buf.Span.Slice(LzhsHeader.SIZE, chunk.outerHeader.uncompressedSize);
+                var raw = chunk.SegmentData.Span.Slice(LzhsHeader.SIZE, chunk.SegmentHeader.UncompressedSize);
                 raw.CopyTo(ptrOut);
                 return;
             }
@@ -175,8 +165,7 @@ namespace Yafex.FileFormats.LzhsFs
                 writer.WriteData(rdr.GetUncompressedHeading(), 0);
                 Parallel.ForEach(rdr.GetChunks(), chunk =>
                 {
-                    log.Info($"Extracting chunk {chunk.index} -> 0x{chunk.outputOffset:X8} (0x{chunk.size:X8}) {(chunk.isUncompressed ? "[UNCOMPRESSED]" : "")}");
-                    log.Info($"    0x{chunk.Header.compressedSize:X8} -> 0x{chunk.outerHeader.uncompressedSize:X8}");
+                    log.Info($"[{chunk.InputOffset:X8}]: Extracting chunk {chunk.SegmentIndex} -> 0x{chunk.OutputOffset:X8} (0x{chunk.SegmentSize:X8}) {(chunk.IsUncompressed ? "[UNCOMPRESSED]" : "")}");
                     writer.WriteChunk(chunk);
                 });
             }

Yafex/FileFormats/LzhsFs/LzhsFsReader.cs
@@ -8,19 +8,61 @@
  *  3. This notice may not be removed or altered from any source distribution.
  */
 #endregion
-using Yafex.FileFormats.Lzhs;
-
 using System;
+using System.Runtime.CompilerServices;
+
 using Smx.SharpIO.Memory.Buffers;
+
+using Yafex.FileFormats.Lzhs;
 
 namespace Yafex.FileFormats.LzhsFs
 {
-    public record LzhsChunk(ushort index, int size, long outputOffset, LzhsHeader outerHeader, Memory64<byte> buf)
+    public class LzhsChunk
     {
-        public readonly LzhsHeader Header = new LzhsHeader(buf.Span);
-        public LzhsDecoder NewDecoder() => new LzhsDecoder(buf);
-        public bool isUncompressed => 
-            Header.compressedSize == Header.uncompressedSize &&
-            Header.checksum == 0x00;
+        public readonly LzhsHeader SegmentHeader;
+        public readonly LzhsHeader Header;
+        public readonly ReadOnlyMemory64<byte> SegmentData;
+        public readonly long InputOffset;
+        public readonly long OutputOffset;
+
+        private static readonly int HEADER_SIZE = Unsafe.SizeOf<LzhsHeader>();
+
+        private uint Pad(uint num, uint align = 16) {
+            uint rem = num % align;
+            uint pad = rem switch {
+                0 => 0,
+                _ => 16 - rem
+            };
+            return num + pad; 
+        }
+
+        public long SizeCompressed => Header.CompressedSize;
+        public long SizeUncompressed => Header.UncompressedSize;
+
+        public ushort Checksum => Header.Checksum;
+
+
+        public bool IsUncompressed =>
+            Header.CompressedSize == Header.UncompressedSize &&
+            Header.Checksum == 0x00;
+
+        public long SegmentIndex => SegmentHeader.Checksum;
+        public bool IsCompressed => !IsUncompressed;
+        public long SegmentSize => HEADER_SIZE + Pad(SegmentHeader.CompressedSize);
+
+        public LzhsChunk(ReadOnlyMemory64<byte> data, long offsetIn, long offsetOut)
+        {
+            InputOffset = offsetIn;
+            OutputOffset = offsetOut;
+
+            var pos = offsetIn;
+            SegmentHeader = new LzhsHeader(data.Slice(pos, HEADER_SIZE).Span);
+            pos += HEADER_SIZE;
+
+            SegmentData = data.Slice(pos, HEADER_SIZE + SegmentSize);
+            Header = new LzhsHeader(data.Slice(pos, HEADER_SIZE).Span);
+        }
+
+        public LzhsDecoder NewDecoder() => new LzhsDecoder(SegmentData);
     }
 }

Yafex/FileFormats/Lzhs/LzhsDecoder.cs
@@ -23,10 +23,10 @@ namespace Yafex.FileFormats.Lzhs
 {
     public unsafe struct LzhsHeader
     {
-        public uint uncompressedSize;
-        public uint compressedSize;
-        public ushort checksum;
-        public fixed byte spare[6];
+        public uint UncompressedSize;
+        public uint CompressedSize;
+        public ushort Checksum;
+        private fixed byte spare[6];
 
         public static int SIZE => sizeof(LzhsHeader);
 
@@ -172,7 +172,7 @@ namespace Yafex.FileFormats.Lzhs
 
         public bool VerifyChecksum()
         {
-            return checksum.Value == header.checksum;
+            return checksum.Value == header.Checksum;
         }
 
         private uint Pad(uint num, uint align = 16)
@@ -189,12 +189,12 @@ namespace Yafex.FileFormats.Lzhs
         public IEnumerable<byte> AsEnumerable()
         {
             var huffmanIn = data.Skip(Marshal.SizeOf<LzhsHeader>())
-                .Take((int)header.compressedSize);
+                .Take((int)header.CompressedSize);
 
             var huffmanOut = new HuffmanDecoder(huffmanIn).Decode();
             var lzssOut = new LzssDecoder(huffmanOut).Decode();
             var athumbOut = new ArmThumbConvert(lzssOut).Decode(0);
-            var output = athumbOut.Take((int)header.uncompressedSize);
+            var output = athumbOut.Take((int)header.UncompressedSize);
             var final = checksum.Update(output);
             return final;
         }

Yafex/FileFormats/LzhsFs/LzhsFsDetector.cs
@@ -32,20 +32,20 @@ namespace Yafex.FileFormats.LzhsFs
             try
             {
                 LzhsHeader firstHdr = new LzhsHeader(source.Data.Span.Slice(LzhsFsReader.UNCOMPRESSED_HEADING_SIZE, 16));
-                if (firstHdr.checksum != 1) throw new InvalidDataException();
+                if (firstHdr.Checksum != 1) throw new InvalidDataException();
 
                 var innerData = source.Data.Slice(LzhsFsReader.UNCOMPRESSED_HEADING_SIZE + 16);
                 var dec = new LzhsDecoder(innerData);
                 dec.AsEnumerable().ToArray();
                 bool result = dec.VerifyChecksum();
-                Console.WriteLine($"{firstHdr.checksum}: " + (result ? "PASS" : "FAIL"));
+                Console.WriteLine($"{firstHdr.Checksum}: " + (result ? "PASS" : "FAIL"));
 
                 if (result) {
                     return new DetectionResult(80, null);
                 }
 
                 //Decompression failed, but we want to check in case the first segment is stored uncompressed
-                if (dec.header.checksum == 0x00 && dec.header.compressedSize == dec.header.uncompressedSize)
+                if (dec.header.Checksum == 0x00 && dec.header.CompressedSize == dec.header.UncompressedSize)
                 {
                     return new DetectionResult(50, null);
                 }

commit fb03dd33aadaf3ddd962662538f6a7c1ce2d35ef

Lzhs: fix zero-filled segments (broken by 5ce3eb3957e99ee1e66ca1c2c9cb61fdd559e391)

Yafex/FileFormats/LzhsFs/LzhsFsExtractor.cs
@@ -76,10 +76,6 @@ namespace Yafex.FileFormats.LzhsFs
             while (inOffset < data.Length)
             {
                 var chunk = new LzhsChunk(data, inOffset, outOffset);
-                if(chunk.SizeCompressed == 0)
-                {
-                    yield break;
-                }
                 inOffset += chunk.SegmentSize;
                 outOffset += chunk.SizeUncompressed;
                 yield return chunk;
@@ -163,11 +159,20 @@ namespace Yafex.FileFormats.LzhsFs
             using (var writer = new LzhsFsWriter(destPath, rdr.GetOutputSize()))
             {
                 writer.WriteData(rdr.GetUncompressedHeading(), 0);
-                Parallel.ForEach(rdr.GetChunks(), chunk =>
+                var chunks = rdr.GetChunks();
+#if LZHS_DEBUG
+                foreach(var chunk in chunks)
+                {
+                    log.Info($"[{chunk.InputOffset:X8}]: Extracting chunk {chunk.SegmentIndex} -> 0x{chunk.OutputOffset:X8} (0x{chunk.SegmentSize:X8}) {(chunk.IsUncompressed ? "[UNCOMPRESSED]" : "")}");
+                    writer.WriteChunk(chunk);
+                }
+#else
+                Parallel.ForEach(chunks, chunk =>
                 {
                     log.Info($"[{chunk.InputOffset:X8}]: Extracting chunk {chunk.SegmentIndex} -> 0x{chunk.OutputOffset:X8} (0x{chunk.SegmentSize:X8}) {(chunk.IsUncompressed ? "[UNCOMPRESSED]" : "")}");
                     writer.WriteChunk(chunk);
                 });
+#endif
             }
 
             return Enumerable.Empty<IDataSource>();

Yafex/FileFormats/LzhsFs/LzhsFsReader.cs
@@ -37,17 +37,24 @@ namespace Yafex.FileFormats.LzhsFs
         }
 
         public long SizeCompressed => Header.CompressedSize;
-        public long SizeUncompressed => Header.UncompressedSize;
+        public long SizeUncompressed => SegmentHeader.UncompressedSize;
 
         public ushort Checksum => Header.Checksum;
-
 
         public bool IsUncompressed =>
             Header.CompressedSize == Header.UncompressedSize &&
             Header.Checksum == 0x00;
 
+        public bool IsZeroFill => 
+            Header.CompressedSize == 0 && Header.UncompressedSize > 0 &&
+            Header.Checksum == 0x00;
+
         public long SegmentIndex => SegmentHeader.Checksum;
         public bool IsCompressed => !IsUncompressed;
+        
+        /// <summary>
+        /// Size of the inner LZHS + the outer lzhs header
+        /// </summary>
         public long SegmentSize => HEADER_SIZE + Pad(SegmentHeader.CompressedSize);
 
         public LzhsChunk(ReadOnlyMemory64<byte> data, long offsetIn, long offsetOut)
@@ -59,10 +66,16 @@ namespace Yafex.FileFormats.LzhsFs
             SegmentHeader = new LzhsHeader(data.Slice(pos, HEADER_SIZE).Span);
             pos += HEADER_SIZE;
 
-            SegmentData = data.Slice(pos, HEADER_SIZE + SegmentSize);
             Header = new LzhsHeader(data.Slice(pos, HEADER_SIZE).Span);
+
+            SegmentData = IsZeroFill
+                ? new byte[SegmentHeader.UncompressedSize]
+                : data.Slice(pos, HEADER_SIZE + SegmentSize);
         }
 
-        public LzhsDecoder NewDecoder() => new LzhsDecoder(SegmentData);
+        public LzhsDecoder? NewDecoder()
+        {
+            return new LzhsDecoder(SegmentData);
+        }
     }
 }